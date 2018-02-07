f7b7c737cd7357a58427e892ab51883df27e1740
davidyunius <32894219+davidyunius@users.noreply.github.com>
2018-02-07 16:48:07 +0700

    release 0

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ function convertString(str) {
     let result = []
     let splitStr = str.split(',')
     for (let i = 0; i < splitStr.length; i++) {
-        result.push(splitStr[i].split(''))
+        let arr = []
+        for (let j = 0; j < splitStr[i].length; j++) {
+            arr.push(splitStr[i][j])
+        }
+        result.push(arr)
     }
     return result
 }

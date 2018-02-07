5d8402a2352b9cc0561bf288a7501c1dbaf571b4
marcosumali <marco.sumali90@gmail.com>
2018-02-07 15:01:56 +0700

    add hasil

---
+++ index.js
@@ -0,0 +1,26 @@
+function strToArr(setering) {
+
+    // console.log(setering); // aqrst,ukaei,ffooo
+    var arrSplit = setering.split(',')
+    // console.log(arrSplit); // [ 'aqrst', 'ukaei', 'ffooo' ]
+    // console.log(arrSplit.length); // 3
+
+    var arrHasil = [];
+    for (var i = 0; i < arrSplit.length; i++) {
+        arrHasil.push([]);
+        var input = arrSplit[i].split('');
+        // console.log(i, input);
+        // console.log(input.length); // 5
+
+        for (var j = 0; j < input.length; j++) {
+            arrHasil[i].push(input[j]);
+        }
+    }
+
+    return arrHasil;
+
+}
+
+
+console.log(strToArr('aqrst,ukaei,ffooo'));
+// expected output each string separated by , to inserted to array
